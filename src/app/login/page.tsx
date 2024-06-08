10eb22d3aa9405d297c91b9af8a98d371fdf9dd2
daxchx <daxchx.v1@gmail.com>
2024-06-08 14:18:50 +0900

    feat: サインイン機能を追加する (#14)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,42 @@
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
+import { createClient } from '@/utils/supabase/server'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const db = createClient()
+  const {
+    data: { user },
+  } = await db.auth.getUser()
+
+  const signIn = async () => {
+    'use server'
+    const db = createClient()
+    const origin = headers().get('origin')
+    const { data, error } = await db.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${origin}/auth/callback`,
+      },
+    })
+
+    if (error) {
+      console.error(error)
+    } else {
+      return redirect(data.url)
+    }
+  }
+
   return (
     <>
       <h1>this is login page!</h1>
-      <Button asChild>
-        <Link href="/dashboard">login</Link>
-      </Button>
+      <div className="flex min-h-screen items-center justify-center">
+        <form action={signIn}>
+          <Button>login</Button>
+        </form>
+      </div>
     </>
   )
 }
