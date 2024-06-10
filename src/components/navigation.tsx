a2856d533fed13b49631521f509c5327423b3fc5
daxchx <daxchx.v1@gmail.com>
2024-06-11 00:16:16 +0900

    feat: データベースと統合する (#18)

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,10 @@
-import { createClient } from '@/utils/supabase/server'
 import Link from 'next/link'
-import Image from 'next/image'
+
+import { HomeIcon } from '@radix-ui/react-icons'
+
+import { createClient } from '@/utils/supabase/server'
+import { Products } from '@/components/products'
+import { Account } from '@/components/account'
 
 export const Navigation = async () => {
   const db = createClient()
@@ -8,39 +12,28 @@ export const Navigation = async () => {
     data: { user },
   } = await db.auth.getUser()
 
+  const { data } = await db
+    .from('products')
+    .select()
+    .eq('user_id', user?.id)
+    .order('created_at', { ascending: true })
+
   return (
     <div className="py-4">
-      <div className="flex items-center justify-between px-2 py-1 hover:bg-gray-200">
-        <div className="flex h-6 w-6 items-center justify-center overflow-hidden rounded bg-gray-400">
-          <Image src={user?.user_metadata.avatar_url} alt="" width={40} height={40} />
-        </div>
-        <div>{user?.user_metadata.name}さん</div>
+      <div className="mb-2 px-2">
+        <Account user={user} />
       </div>
-      <form action="/auth/signout" method="post">
-        <button>signout</button>
-      </form>
-      <div className="px-2 py-4">
-        <div className="mb-4">
-          <Link href="/dashboard" className="flex p-2 hover:bg-gray-200">
-            home
-          </Link>
-        </div>
-        <div className="mb-2 text-sm text-gray-600">Projects</div>
-        <div>
-          <Link href="/dashboard/a" className="flex p-2 hover:bg-gray-200">
-            a
-          </Link>
-        </div>
-        <div>
-          <Link href="/dashboard/b" className="flex p-2 hover:bg-gray-200">
-            b
-          </Link>
-        </div>
-        <div>
-          <Link href="/dashboard/c" className="flex p-2 hover:bg-gray-200">
-            c
-          </Link>
-        </div>
+      <div className="mb-6 px-2">
+        <Link
+          href="/dashboard"
+          className="flex w-full items-center rounded px-2 py-1 hover:bg-gray-200"
+        >
+          <HomeIcon className="h-4 w-4" />
+          <span className="ml-2 text-sm font-medium">ホーム</span>
+        </Link>
+      </div>
+      <div className="px-2">
+        <Products user={user} data={data ?? []} />
       </div>
     </div>
   )
