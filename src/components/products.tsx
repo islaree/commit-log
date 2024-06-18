2580c75c25aec7ad20a65f85a3fe1d51f02df38d
daxchx <daxchx.v1@gmail.com>
2024-06-18 16:55:16 +0900

    feat: ssr→csrに変更する (#24)

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,203 +1,215 @@
-'use client'
+"use client"
 
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { UUID } from 'crypto'
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { CircleCheckBig, Loader2, Trash2 } from "lucide-react"
+import { UUID } from "crypto"
 
-import { User } from '@supabase/supabase-js'
-import { PlusIcon } from '@radix-ui/react-icons'
-import { DotsHorizontalIcon } from '@radix-ui/react-icons'
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import { TrashIcon } from '@radix-ui/react-icons'
+import { User } from "@supabase/supabase-js"
+import { PlusIcon } from "@radix-ui/react-icons"
+import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import { Pencil2Icon } from "@radix-ui/react-icons"
 
-import { createClient } from '@/utils/supabase/client'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
+import { createClient } from "@/utils/supabase/client"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"  
+  
 
 type Product = {
-  id: UUID
-  user_id: UUID
-  name: string
+    completed: boolean
+    created_at: string
+    id: UUID
+    name: string
+    user_id: UUID
 }
 
-export const Products = ({ data, user }: { data: Product[]; user: User | null }) => {
-  const [products, setProducts] = useState(data)
-  const [value, setValue] = useState('')
+export const Products = ({user}: {user: User | null}) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [products, setProducts] = useState<Product[] | null>()
 
-  const db = createClient()
+    const db = createClient()
 
-  const handleInsert = async () => {
-    setValue('')
-    if (value.length > 0) {
-      await db.from('products').insert({
-        user_id: user?.id,
-        name: value,
-      })
-    }
-  }
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await db.from("products")
+            .select()
+            .eq("user_id", user?.id)
+            .order('created_at', { ascending: true })
+            setProducts(data)
+            setIsLoading(false)
+        } 
+        getData()
+    }, [db, user?.id])
 
-  useEffect(() => {
-    const channel = db
-      .channel('realtime products')
-      .on(
-        'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'products',
+    useEffect(() => {
+        const channel = db.channel("realtime products").on("postgres_changes", {
+            event: "*",
+            schema: "public",
+            table: "products"
         },
         (payload) => {
-          switch (payload.eventType) {
-            case 'INSERT':
-              setProducts([...products, payload.new as Product])
-              break
-            case 'UPDATE':
-              setProducts(
-                products.map((p) => {
-                  if (p.id == payload.new.id)
-                    return { id: p.id, name: payload.new.name, user_id: payload.new.user_id }
-                  else return p
-                }),
-              )
-              break
-            case 'DELETE':
-              setProducts(products.filter((product) => product.id !== payload.old.id))
-              break
-            default:
-              return
-          }
-        },
-      )
-      .subscribe()
+            switch(payload.eventType) {
+                case "INSERT":
+                    setProducts([...products!, payload.new as Product])
+                    break;
+                case "UPDATE":
+                    setProducts(products!.map(product => {
+                        if(product.id == payload.new.id) return payload.new as Product
+                        else return product
+                    }))
+                    break;
+                case "DELETE": 
+                    setProducts(products!.filter(product => product.id !== payload.old.id))
+                    break;
+                default: return
+            }
+        }).subscribe()
 
-    return () => {
-      db.removeChannel(channel)
-    }
-  }, [db, products, setProducts])
+        return () => {
+            db.removeChannel(channel)
+        }
 
-  return (
-    <>
-      <div className="mb-2 flex items-center justify-between border-b border-gray-300 pb-1 pl-2 pr-1.5">
-        <div className="text-xs font-semibold text-gray-600">あなたのプロダクト</div>
-        <Dialog>
-          <DialogTrigger>
-            <PlusIcon className="h-6 w-6 rounded p-1 hover:bg-gray-200" />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Add products</DialogTitle>
-              <DialogDescription>
-                <Input
-                  id="product-name"
-                  className="col-span-3"
-                  value={value}
-                  placeholder="product name"
-                  onChange={(e) => setValue(e.target.value)}
-                />
-              </DialogDescription>
-            </DialogHeader>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button type="button" onClick={handleInsert}>
-                  submit
-                </Button>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
-      <div>
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="flex items-center justify-between rounded pr-1.5 hover:bg-gray-200"
-          >
-            <Link href={`/dashboard/${product.id}`} className="w-full py-1.5 pl-2 text-sm">
-              {product.name}
-            </Link>
-            <ProductItem data={product} user={user} />
-          </div>
-        ))}
-      </div>
-    </>
-  )
+    }, [db, products, setProducts])
+
+    return (
+        <>
+            <div className="flex items-center justify-between mb-2 pb-1 border-b border-gray-300 pl-2 pr-1.5">
+                <div className="text-xs text-gray-600 font-semibold">あなたのプロダクト</div>
+                <CreateProduct user={user} />
+            </div>
+            <div>
+                {isLoading ? (
+                    <div className="py-1.5 px-2">
+                        <div className="flex flex-col justify-center items-center">
+                            <Loader2 className="h-6 w-6 animate-spin" />
+                            <span className="mt-1 text-sm">Loading</span>
+                        </div>
+                    </div>
+                ) : (
+                    products?.map(product => (
+                        <div key={product.id} className="flex items-center justify-between pr-1.5 rounded hover:bg-gray-100">
+                            <Link href={`/dashboard/${product.id}`} className="flex gap-x-2 items-center text-sm w-full pl-2 py-1.5">
+                                {product.completed && (
+                                    <CircleCheckBig className="text-emerald-600 w-4 h-4" />
+                                )}{product.name}
+                            </Link>
+                            <ProductItem product={product} user={user} />
+                        </div>
+                    ))
+                )}
+            </div>
+        </>
+    )
 }
 
-function ProductItem({ data, user }: { data: Product; user: User | null }) {
-  const [productName, setProductName] = useState(data.name)
+function ProductItem({product, user}: {product: Product, user: User | null}) {
+    const [productName, setProductName] = useState(product.name)
 
-  const db = createClient()
+    const db = createClient()
 
-  const handleUpdate = async (id: UUID) => {
-    await db
-      .from('products')
-      .upsert({
-        id,
-        name: productName,
-        user_id: user?.id,
-      })
-      .eq('id', id)
-  }
+    const handleUpdate = async (id: UUID) => {
+        await db.from("products").upsert({
+            id,
+            name: productName,
+            user_id: user?.id,
+        }).eq("id", id)
+    }
 
-  const handleDelete = async (id: UUID) => {
-    await db.from('products').delete().eq('id', id)
-  }
+    const handleDelete = async (id: UUID) => {
+        await db.from("products").delete().eq("id", id)
+    }
 
-  return (
-    <>
-      <Dialog>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <DotsHorizontalIcon className="h-6 w-6 cursor-pointer rounded p-1 hover:bg-gray-300" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DialogTrigger asChild>
-              <DropdownMenuItem className="flex items-center gap-x-2">
-                <Pencil2Icon className="h-4 w-4" />
-                <span className="text-sm">名前を変更</span>
-              </DropdownMenuItem>
+    return (
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <DotsHorizontalIcon className="h-6 w-6 p-1 rounded cursor-pointer hover:bg-gray-300" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DialogTrigger onClick={() => setProductName(product.name)} asChild>
+                        <DropdownMenuItem className="flex items-center gap-x-2 cursor-pointer">
+                            <Pencil2Icon className="h-4 w-4" />
+                            <span className="text-sm">名前を変更</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="flex items-center gap-x-2 cursor-pointer text-red-600" onClick={() => handleDelete(product.id)}>
+                        <Trash2 className="h-4 w-4" />
+                        <span className="text-sm">削除</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>名前を変更する</DialogTitle>
+                    <DialogDescription>
+                        <Input value={productName} onChange={e => setProductName(e.target.value)} />
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button type="button" onClick={() => handleUpdate(product.id)}>更新する</Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+const CreateProduct = ({user}: {user: User | null}) => {
+    const [value, setValue] = useState("")
+
+    const db = createClient()
+
+    const handleInsert = async () => {
+        setValue("")
+        if(value.length > 0){
+            await db.from("products").insert({
+                user_id: user?.id,
+                name: value
+            })
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            setValue("")
+        }
+    }, [])
+
+    return (
+        <Dialog>
+            <DialogTrigger onClick={() => setValue("")}>
+                <PlusIcon className="h-6 w-6 p-1 rounded hover:bg-gray-200" />
             </DialogTrigger>
-            <DropdownMenuItem
-              className="flex items-center gap-x-2"
-              onClick={() => handleDelete(data.id)}
-            >
-              <TrashIcon className="h-4 w-4" />
-              <span className="text-sm">削除</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              <Input value={productName} onChange={(e) => setProductName(e.target.value)} />
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button type="button" onClick={() => handleUpdate(data.id)}>
-                Confirm
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
-  )
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>新規プロダクトを追加する</DialogTitle>
+                    <DialogDescription>
+                        <Input id="product-name" className="col-span-3" value={value} placeholder="プロダクト名" onChange={e => setValue(e.target.value)}/>          
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                <DialogClose asChild>
+                    <Button type="button" onClick={handleInsert}>追加する</Button>
+                </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
 }
