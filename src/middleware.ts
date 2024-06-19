cab3acc0cdc22680b1b883b8f33fa55b66ece10f
daxchx <daxchx.v1@gmail.com>
2024-06-19 13:37:27 +0900

    add: ミドルウェアを追加する

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+
+export async function middleware(request: NextRequest) {
+  const db = createClient()
+  const {
+    data: { user },
+  } = await db.auth.getUser()
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth/callback')
+  ) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
