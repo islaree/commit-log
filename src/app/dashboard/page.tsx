418f17f61d33b46699a94cd17fc51241d2e09d80
daxchx <daxchx.v1@gmail.com>
2024-06-26 17:30:38 +0900

    feat: cache機能を追加する (#41)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,29 @@
+import { cache } from 'react'
+
 import { Account } from '@/components/account'
 import { Products } from '@/components/products'
 import { createClient } from '@/utils/supabase/server'
 
-export default async function Dashboard() {
+const getUser = cache(async () => {
   const db = createClient()
   const {
     data: { user },
   } = await db.auth.getUser()
 
+  return user
+})
+
+export default async function Dashboard() {
+  const user = await getUser()
+
   return (
-    <>
+    <div className="p-4">
       <div className="flex justify-end">
         <Account user={user} />
       </div>
       <div className="mt-6 flex flex-col gap-y-4">
         <Products user={user} />
       </div>
-    </>
+    </div>
   )
 }
