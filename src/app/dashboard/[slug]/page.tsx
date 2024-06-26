8c916ad0f070bd89a2354de8f39321dafbd0eba1
daxchx <daxchx.v1@gmail.com>
2024-06-26 17:32:51 +0900

    feat: データを取得する (#41)

--- src/app/dashboard/[slug]/page.tsx
+++ src/app/dashboard/[slug]/page.tsx
@@ -1,11 +1,25 @@
 import { Messages } from '@/components/messages'
-import { ProductHeader } from '@/components/product-header'
+import { Product } from '@/components/product'
+import { createClient } from '@/utils/supabase/server'
+import { UUID } from 'crypto'
+import { ChevronLeft } from 'lucide-react'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import { cache } from 'react'
 
-export default function ProductPage({ params }: { params: { slug: string } }) {
+export default async function ProductPage({ params }: { params: { slug: string } }) {
+  const db = createClient()
+  const { data, error } = await db.from('products').select().eq('id', params.slug).single()
+  if (error) {
+    notFound()
+  }
+
   return (
-    <div className="relative h-screen">
-      <ProductHeader id={params.slug} />
-      <Messages productId={params.slug} />
-    </div>
+    <>
+      <div className="mb-6 mt-4 px-4">
+        <Product id={params.slug} />
+      </div>
+      <Messages />
+    </>
   )
 }
