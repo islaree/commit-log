e5fb50b5910d85a9865d4e32c3638ea2394a7765
daxchx <daxchx.v1@gmail.com>
2024-06-27 10:33:28 +0900

    remove: 不要なコードを削除する (#46)

--- src/app/dashboard/[slug]/page.tsx
+++ src/app/dashboard/[slug]/page.tsx
@@ -1,19 +1,7 @@
 import { Messages } from '@/components/messages'
 import { Product } from '@/components/product'
-import { createClient } from '@/utils/supabase/server'
-import { UUID } from 'crypto'
-import { ChevronLeft } from 'lucide-react'
-import Link from 'next/link'
-import { notFound } from 'next/navigation'
-import { cache } from 'react'
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
-  const db = createClient()
-  const { data, error } = await db.from('products').select().eq('id', params.slug).single()
-  if (error) {
-    notFound()
-  }
-
+export default function ProductPage({ params }: { params: { slug: string } }) {
   return (
     <>
       <div className="mb-6 mt-4 px-4">
