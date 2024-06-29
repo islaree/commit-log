419a8b3174e837954899bdb6757c0f97b0caf57f
daxchx <daxchx.v1@gmail.com>
2024-06-29 17:33:26 +0900

    remove: 関数を削除する (#49)

--- src/db/posts.ts
+++ src/db/posts.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@/utils/supabase/server'
-import { format } from 'date-fns'
-import { revalidatePath } from 'next/cache'
 
 export async function getPosts(id: string) {
   const db = createClient()
@@ -10,15 +8,4 @@ export async function getPosts(id: string) {
     .eq('product_id', id)
     .order('created_at', { ascending: false })
   return data
-}
-
-export async function createPost(id: string, message: string) {
-  const db = createClient()
-  await db.from('commits').insert({
-    product_id: id,
-    message: message,
-    created_at: format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXXX"),
-  })
-
-  revalidatePath(`/dashboard/${id}`)
 }
